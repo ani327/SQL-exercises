5b76ca66a5cfe9d1ce3879835fe4020fd0cec254
animiraqyan <83648841+ani327@users.noreply.github.com>
2022-10-08 16:42:25 +0400

    Update Exercise20.sql

--- Exercise20.sql
+++ Exercise20.sql
@@ -1,5 +1,5 @@
 #Get product which budget is most expensive one (qty*unit_price) and has lexicographically smallest name. Result should be one row
 use sql_store;
 select *, quantity_in_stock * Unit_price as budget from products
-order by name and budget desc
+order by name, budget desc
 limit 1
